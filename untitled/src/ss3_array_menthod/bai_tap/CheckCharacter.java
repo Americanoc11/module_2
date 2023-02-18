231d9c34a7045baf1c3d705d9d1447c8535fe3d7
americanoc11 <hhd0511469699@gmail.com>
2023-02-18 11:29:35 +0700

    update

--- untitled/src/ss3_array_menthod/bai_tap/CheckCharacter.java
+++ untitled/src/ss3_array_menthod/bai_tap/CheckCharacter.java
@@ -1,4 +1,23 @@
 package ss3_array_menthod.bai_tap;
 
+import java.util.Scanner;
+
 public class CheckCharacter {
+    public static void main(String[] args) {
+        String[] students = {"Danh", "Duc", "Tuan", "Tri"};
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter a name's student: ");
+        String inputName = input.nextLine();
+        boolean isExit = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(inputName)) {
+                System.out.println("Tên nằm trong danh sách: " + inputName + " vị trí " + (i + 1));
+                isExit = true;
+                break;
+            }
+        }
+        if (isExit == false) {
+            System.out.println(" không tìm thấy " + inputName + " trong danh sách");
+        }
+    }
 }
